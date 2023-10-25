44639d9a3dd2213524cb61bec0fd66b289297aff
Nursultan <tupoukidala77@gmail.com>
2023-10-25 20:10:15 +0600

    js1

---
+++ script.js
@@ -0,0 +1,27 @@
+// let a =4
+// let b =55
+// console.log(a+b)
+
+// let a =3
+// let b =45
+// console.log(b-a)
+
+// let a ="Hello "
+// let b ="Bro"
+// console.log(b+a)
+
+// let a ="Nursultan "
+// let b ="Hello"
+// console.log(b + a)
+
+// let a =2
+// let b ="Hello"
+// console.log(b - a)
+
+// let a =5
+// let b ="5"
+// console.log(+b + a)
+
+// let c =2
+// let b =
+// console.log(c+ d)
